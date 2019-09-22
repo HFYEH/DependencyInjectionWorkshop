3d94a52182e27c1704af419a14b6c6a5c3569f0d
HFYEH <sharefun010407@gmail.com>
2019-09-22 11:21:45 +0800

    Move

--- DependencyInjectionWorkshop/Models/NotificationDecorator.cs
+++ DependencyInjectionWorkshop/Models/NotificationDecorator.cs
@@ -1,20 +1,5 @@
 namespace DependencyInjectionWorkshop.Models
 {
-    public class AuthenticationBaseDecorator : IAuthentication
-    {
-        private readonly IAuthentication _authentication;
-
-        protected AuthenticationBaseDecorator(IAuthentication authentication)
-        {
-            _authentication = authentication;
-        }
-
-        public virtual bool Verify(string accountId, string password, string otp)
-        {
-            return _authentication.Verify(accountId, password, otp);
-        }
-    }
-
     public class NotificationDecorator : AuthenticationBaseDecorator
     {
         private readonly INotification _notification;
